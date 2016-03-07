5e939bb653193b38d9c2aa528774028d033698bc
CatherineWong <wong.catherine.mail@gmail.com>
2016-03-07 00:15:29 -0800

    Save results as text map

--- Sentry/ViewController.swift
+++ Sentry/ViewController.swift
@@ -37,7 +37,7 @@ extension ViewController : ORKTaskViewControllerDelegate {
         taskViewController.dismissViewControllerAnimated(true, completion: nil)
         
         // On form completion, send the results to a database
-        let results = []
+        var resultsMap = [String: String]()
         if let stepResults = taskViewController.result.results as? [ORKStepResult] {
             // TODO: clean up; consider not hardcoding
             for stepResult in stepResults {
@@ -45,27 +45,37 @@ extension ViewController : ORKTaskViewControllerDelegate {
                 if stepIdentifier == "ImageCaptureStep" {
                     print(stepIdentifier)
                     print(stepResult.results)
-                    results[stepIdentifier]
                 } else if stepIdentifier == "LesionTopThreeLabelingStep" {
                     print(stepIdentifier)
                     print(stepResult.results)
+                    for lesionTopThreeResult in stepResult.results! as! [ORKTextQuestionResult] {
+                        resultsMap[lesionTopThreeResult.identifier] = lesionTopThreeResult.answer as! String
+                    }
                 } else if stepIdentifier == "BiopsyPatientInformationStep" {
                     print(stepIdentifier)
                     print(stepResult.results)
+                    for questionResult in stepResult.results! as! [ORKTextQuestionResult] {
+                        resultsMap[questionResult.identifier] = questionResult.answer as! String
+                    }
+                    
                 }
             }
+            
         } else {
             // no results
             print("No results!")
         }
         
+        print(resultsMap)
+        
         // Send to an azure database
+        
         self.client = MSClient(
             applicationURLString:"https://thrunresearch.azure-mobile.net/",
             applicationKey:"YgzTPXIjkRWuXbkmaczgleMufrWqWy99"
         )
         
-        let item = ["text":"Awesome item"]
+        let item = resultsMap
         let itemTable = self.client!.tableWithName("Item")
         itemTable.insert(item) {
             (insertedItem, error) in
